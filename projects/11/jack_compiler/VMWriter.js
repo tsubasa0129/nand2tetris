d3250b9fa4b75b0bfbd478b8fc27930f15887ffd
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2022-07-31 19:11:26 +0900

    create basis

---
+++ projects/11/jack_compiler/VMWriter.js
@@ -0,0 +1,45 @@
+class VMWriter {
+    constructor(){
+
+    }
+
+    writePush(Segment,Index){
+
+    }
+
+    writePop(Segment,Index){
+
+    }
+
+    writeArithmetic(command){
+
+    }
+
+    writeLabel(label){
+
+    }
+
+    writeGoto(label){
+
+    }
+
+    writeIf(label){
+
+    }
+
+    writeCall(name,nArgs){
+
+    }
+
+    writeFunction(name,nLocals){
+
+    }
+
+    writeReturn(){
+
+    }
+
+    close(){
+        
+    }
+}
